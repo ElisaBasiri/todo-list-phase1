03ccd5cfaae75be11437f8529574750986ca88e1
ElisaBasiri <elisabasiri@gmail.com>
2026-02-11 20:31:52 +0330

    feat(models): introduce basic Task class

--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
+from datetime import date
 from enum import Enum
+from typing import List, Optional
 
 
 class TaskStatus(Enum):
@@ -6,3 +8,22 @@ class TaskStatus(Enum):
     TODO = "todo"
     DOING = "doing"
     DONE = "done"
+
+
+class Task:
+    """Model representing a single task inside a project"""
+
+    def __init__(
+        self,
+        title: str,
+        description: str = "",
+        status: TaskStatus = TaskStatus.TODO,
+        deadline: Optional[date] = None,
+        task_id: Optional[int] = None,
+    ):
+        
+        self.task_id: Optional[int] = task_id
+        self.title: str = title.strip()
+        self.description: str = description.strip()
+        self.status: TaskStatus = status
+        self.deadline: Optional[date] = deadline
