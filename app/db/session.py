5a464f8afb8b2a0a5529fe2d2b6d445fa2be6859
ElisaBasiri <elisabasiri@gmail.com>
2026-02-12 16:40:55 +0330

    feat(phase2):add database base and session mangement with DI support

--- app/db/session.py
+++ app/db/session.py
@@ -1,1 +1,15 @@
- 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from os import getenv
+
+DATABASE_URL = getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=getenv("SQLALCHEMY_ECHO", "false").lower() == "true")
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
